9e666062f87df69ede288d71505bd537ac164712
MrKeltt <dmitriydavidenkokeltt@gmail.com>
2023-03-16 20:06:47 +0300

    Create java spring template

---
+++ src/main/java/ru/vsu/csd/datatransferservice/DataTransferServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.csd.datatransferservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataTransferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataTransferServiceApplication.class, args);
+	}
+
+}
