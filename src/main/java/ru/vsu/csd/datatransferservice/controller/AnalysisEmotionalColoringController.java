971e2829613784e333d4136cd2382233205fe562
MrKeltt <dmitriydavidenkokeltt@gmail.com>
2023-03-31 11:19:22 +0300

    Correction of errors

--- src/main/java/ru/vsu/csd/datatransferservice/controller/AnalysisEmotionalColoringController.java
+++ src/main/java/ru/vsu/csd/datatransferservice/controller/AnalysisEmotionalColoringController.java
@@ -1,10 +1,14 @@
 package ru.vsu.csd.datatransferservice.controller;
 
+import org.python.util.PythonInterpreter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Random;
 
 @RestController
@@ -12,39 +16,37 @@ import java.util.Random;
 public class AnalysisEmotionalColoringController {
     @GetMapping("{text}")
     public double[] doAnalysis(@PathVariable String text) {
-//        try {
-//            Process process = Runtime.getRuntime().exec("pip install --user -r requirements.txt");
-//
-//            BufferedReader inputReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-//            String inputLine;
-//            while ((inputLine = inputReader.readLine()) != null) {
-//                System.out.println(inputLine);
-//            }
-//
-//            BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-//            String errorLine;
-//            while ((errorLine = errorReader.readLine()) != null) {
-//                System.err.println(errorLine);
-//            }
-//
-//            int exitValue = process.waitFor();
-//
-//            if (exitValue == 0) {
-//                // Dependencies installed successfully, now execute the script
-//                PythonInterpreter.initialize(System.getProperties(), System.getProperties(), new String[0]);
-//                PythonInterpreter interpreter = new PythonInterpreter();
-//                interpreter.execfile("src/main/resources/scripts/analysis-emotional-coloring.py");
-//                return "Python script executed successfully";
-//            } else {
-//                // Error installing dependencies
-//                return "Error installing Python dependencies";
-//            }
-//        } catch (IOException e) {
-//                e.printStackTrace();
-//                return "Error occurred during script execution";
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
+        //try {
+            //    Process process = Runtime.getRuntime().exec("pip install --user -r requirements.txt");
+
+            //    BufferedReader inputReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            //    String inputLine;
+            //    while ((inputLine = inputReader.readLine()) != null) {
+                //        System.out.println(inputLine);
+                //    }
+
+            //    BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            //    String errorLine;
+            //    while ((errorLine = errorReader.readLine()) != null) {
+                //        System.err.println(errorLine);
+                //    }
+
+            //    int exitValue = process.waitFor();
+
+            //    if (exitValue == 0) {
+                //        // Dependencies installed successfully, now execute the script
+                //        PythonInterpreter.initialize(System.getProperties(), System.getProperties(), new String[0]);
+                //        PythonInterpreter interpreter = new PythonInterpreter();
+                //        interpreter.execfile("src/main/resources/scripts/analysis-emotional-coloring.py");
+                //        System.out.println("Python script executed successfully");
+                //    } else {
+                //        // Error installing dependencies
+                //        System.out.println("Error installing Python dependencies");
+                //    }
+            //} catch (Exception e) {
+            //    e.printStackTrace();
+            //    throw new RuntimeException(e);
+            //}
 
         Random random = new Random();
         double[] randomArray = new double[5];
